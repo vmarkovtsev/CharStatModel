d4bdf1e3d75347c2fd5c669617b18e6a0567fb06
Vadim Markovtsev <vadim@sourced.tech>
2018-04-16 15:14:06 +0200

    Update the scipts

--- eval.go
+++ eval.go
@@ -43,19 +43,40 @@ func (root node) Eval(prefix string) bool {
 	return false
 }
 
+func reverseStr(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func main() {
 	fileName := os.Args[1]
 	modelFileName := os.Args[2]
+	reverse := false
+	if len(os.Args) > 3 {
+		reverse = true
+	}
 	modelFile, err := os.Open(modelFileName)
 	if err != nil {
 		log.Fatalf("Opening %s: %v", modelFileName, err)
 	}
 	defer modelFile.Close()
 	decoder := gob.NewDecoder(modelFile)
-	root := node{}
-	err = decoder.Decode(&root)
-	if err != nil {
-		log.Fatalf("Reading %s: %v", modelFileName, err)
+	root := node{Children: map[rune]*node{}}
+	for {
+		var key rune
+		var val node
+		err = decoder.Decode(&key)
+		if err != nil {
+			break
+		}
+		err = decoder.Decode(&val)
+		if err != nil {
+			log.Fatalf("Decoding: %v", err)
+		}
+		root.Children[key] = &val
 	}
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -76,12 +97,18 @@ func main() {
 		fails := 0
 		real := 0
 		prefix := ""
-		for i, c := range line[1] {
+		str := line[1]
+		if reverse {
+			str = reverseStr(str)
+		}
+		result := make([]bool, 1, len(str))
+		for i, c := range str {
 			if i == 0 {
 				prefix += string(c)
 				continue
 			}
 			split := root.Eval(prefix)
+			result = append(result, split)
 			if split {
 				if c == ' ' {
 					wins++
@@ -97,10 +124,25 @@ func main() {
 			}
 			prefix += string(c)
 		}
+		if reverse {
+			for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+				result[i], result[j] = result[j], result[i]
+			}
+		}
+		for _, v := range result {
+			var iv int
+			if v {
+				iv = 1
+			}
+			fmt.Printf("%d ", iv)
+		}
+		fmt.Printf("\n")
 		if wins+fails > 0 {
 			precision += float64(wins) / float64(wins+fails)
 		}
-		recall += float64(wins) / float64(real)
+		if real > 0 {
+			recall += float64(wins) / float64(real)
+		}
 	}
 	fmt.Printf("Precision: %f\n", precision/float64(count))
 	fmt.Printf("Recall: %f\n", recall/float64(count))
--- train.go
+++ train.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/gob"
 	"io"
 	"log"
 	"os"
@@ -15,6 +16,9 @@ type node struct {
 }
 
 func (parent *node) GetChild(at rune, alloc *allocator) *node {
+	if parent.Children == nil {
+		parent.Children = map[rune]*node{}
+	}
 	nd, exists := parent.Children[at]
 	if !exists {
 		nd = alloc.New(at)
@@ -47,7 +51,6 @@ func (alloc *allocator) New(char rune) *node {
 	}
 	nd := &chunk.Data[chunk.Pos]
 	nd.Char = char
-	nd.Children = map[rune]*node{}
 	chunk.Pos++
 	return nd
 }
@@ -89,6 +92,53 @@ func applyString(text string, depth int, root *node, alloc *allocator) {
 	}
 }
 
+func prune(root *node) int {
+	stack := []*node{root}
+	count := 0
+	for {
+		if len(stack) == 0 {
+			return count
+		}
+		head := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		count++
+		if head.Children != nil {
+			leaves := true
+			var maxFreq uint64
+			var maxChild *node
+			var maxRune rune
+			for key, child := range head.Children {
+				if child.Children != nil {
+					leaves = false
+					break
+				}
+				if child.Frequency > maxFreq {
+					maxFreq = child.Frequency
+					maxChild = child
+					maxRune = key
+				}
+			}
+			if leaves {
+				head.Children = map[rune]*node{}
+				head.Children[maxRune] = maxChild
+				count++
+			} else {
+				for _, child := range head.Children {
+					stack = append(stack, child)
+				}
+			}
+		}
+	}
+}
+
+func reverseStr(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func main() {
 	alloc := &allocator{}
 	fileName := os.Args[1]
@@ -105,8 +155,16 @@ func main() {
 	var line []string
 	root := node{Children: map[rune]*node{}}
 	i := 0
+	reverse := false
+	if len(os.Args) > 3 {
+		reverse = true
+	}
 	for line, err = reader.Read(); err == nil; line, err = reader.Read() {
-		applyString(line[1], depth, &root, alloc)
+		str := line[1]
+		if reverse {
+			str = reverseStr(str)
+		}
+		applyString(str, depth, &root, alloc)
 		i++
 		if i%100000 == 0 {
 			log.Printf("%d", i)
@@ -116,6 +174,17 @@ func main() {
 	if err != io.EOF {
 		log.Fatalf("Reading CSV: %v", err)
 	}
-	//encoder := gob.NewEncoder(os.Stdout)
-	//encoder.Encode(root)
+	newSize := prune(&root)
+	log.Printf("Pruned to %d - %f\n", newSize, float64(newSize)/float64(alloc.Len()))
+	encoder := gob.NewEncoder(os.Stdout)
+	for key, val := range root.Children {
+		err = encoder.Encode(key)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		err = encoder.Encode(val)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+	}
 }
